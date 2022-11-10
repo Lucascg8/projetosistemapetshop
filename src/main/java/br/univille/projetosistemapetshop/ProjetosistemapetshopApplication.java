dc7f3fc3f26a713be72934e87eecc96fb8e31cda
Lucascg8 <lucascg.design@gmail.com>
2022-11-10 14:48:34 -0300

    projetosistemapetshop

---
+++ src/main/java/br/univille/projetosistemapetshop/ProjetosistemapetshopApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projetosistemapetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosistemapetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosistemapetshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/projetosistemapetshop/ProjetosistemapetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetosistemapetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosistemapetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
